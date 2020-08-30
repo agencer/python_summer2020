eb9f580f76e8eb0cca90299eba7d760da26b9667
Alper Sukru Gencer <alpergencer@wust.edu>
2020-08-30 16:51:10 -0500

    Update hw_gencer.py
    1) Created sellItem method that is inherited in all. Note that it has an extra item_type argument that allows me to change profit based on the type.
    
    2) I have also created bond item.
    
    3) Created a history function that gives a string of all the transactions. I have also added date and time to this string so that the customers could see the detailed timing infos.

--- hw_gencer.py
+++ hw_gencer.py
@@ -1,4 +1,6 @@
 import random
+from datetime import datetime 
+
 
 #	Let's start with the classes for different non liquid investments.
 #		The question asks for inheritence. So let's create one: 
@@ -10,24 +12,29 @@ class NonLiquidItems():
 
 class Stock(NonLiquidItems):
 	def __init__(self, value, name):
-		NonLiquidItems.__init__(self, value, name)
+		NonLiquidItems.__init__(self, int(value), name)
 
 class MutualFund(NonLiquidItems):
 	def __init__(self, name):
 		NonLiquidItems.__init__(self, float(1), name)
+
+class Bond(NonLiquidItems):
+	def __init__(self, value, name):
+		NonLiquidItems.__init__(self, int(value), name)
 
 
 #	Let's continue with our Portfolio class where we have
 #		1) cash attitude
 #		2) stock value attitude
 #		3) mutual_funds value attitude
+	
 
 class Portfolio():
 	def __init__(self):
 		self.cash = float(0)
-		self.stock_dictionary = {}
-		self.mutualfunds_dictionary = {}
+		self.item_dictionary = {}
 		self.value_dictionary = {}
+		self.history_string = "Here is the list of all transactions ordered by time:\n"
 		#	cool! Now let's create the Porfoglio class methods for cash addition, 
 		#		substraction, and the purchase of other items.
 
@@ -36,47 +43,95 @@ class Portfolio():
 			raise TypeError("\nInput should be either integer or float.")
 		else:
 			self.cash += add
-			print("""\n{} unit cash is deposited.\nCurrent deposited unit cash is {}.""".format(add, self.cash))
+			now = datetime.now() 
+			date_time = now.strftime("%d/%m/%Y at %H:%M:%S.%f")
+			output = "{} unit cash is deposited. Current deposited unit cash is {}.\n".format(add, self.cash)
+			self.history_string += f"{date_time},\n{output}"
+
 
 	def withdrawCash(self, remove):
 		if type(remove) not in [int, float]:
-			raise TypeError("Input should be either integer 			or ValueError.")
+			raise TypeError("Input should be either integer or float.")
 		elif self.cash < remove:
 			raise ValueError("The amount desired to be withdrawn cannot be greater than the deposited cash.")
 		else:
 			self.cash -= remove
-			print("""\n{} unit cash is withdrawm.\nCurrent deposited unit cash is {}.""".format(remove, self.cash))
+			output = "{} unit cash is withdrawm. Current deposited unit cash is {}.\n".format(remove, self.cash)
+			now = datetime.now() 
+			date_time = now.strftime("%d/%m/%Y at %H:%M:%S.%f")
+			self.history_string += f"{date_time},\n{output}"
 		#	Yes! Now let's create functions for the stock purchase and sale:
 
-	def  buyStock(self, number, stock):
-		if type(number) not in [int]:
-			raise TypeError("Input should be integer.")
-		elif self.cash < number*stock.value:
-			raise ValueError("The amount desired to be used to purchase stocks cannot be greater than the deposited cash.")
+	def  buyItem(self, number, item):
+		if type(number) not in [int, float]:
+			raise TypeError("Stock input must be integer while mutual funds input must be float.")
+		elif self.cash < number*item.value:
+			raise ValueError("The desired item requires greater unit cash than the deposited cash.")
 		else:
-			self.withdrawCash(number*stock.value)
-			self.value_dictionary.update({stock.name : stock.value})
-			if stock.name in self.stock_dictionary:
-				dict_tempo = {"{}".format(stock.name) : self.stock_dictionary[stock.name] + number}
-				self.stock_dictionary.update(dict_tempo)
+			self.withdrawCash(number*item.value)
+			self.value_dictionary.update({item.name : item.value})
+			if item.name in self.item_dictionary:
+				dict_tempo = {"{}".format(item.name) : self.item_dictionary[item.name] + number}
+				self.item_dictionary.update(dict_tempo)
 			else:
-				dict_tempo = {"{}".format(stock.name) : number}
-				self.stock_dictionary.update(dict_tempo)				
-			print("""\n{} unit cash is spent to buy {} number of {} stock.""".format((number*stock.value), number, stock.name))
+				dict_tempo = {"{}".format(item.name) : number}
+				self.item_dictionary.update(dict_tempo)
+			output = "{} unit cash is spent to buy {} number of {}.\n".format((number*item.value), number, item.name)
+			now = datetime.now() 
+			date_time = now.strftime("%d/%m/%Y at %H:%M:%S.%f")
+			self.history_string += f"{date_time},\n{output}"
 		#	Yes! Now let's create functions for the stock purchase and sale:
 
-	def  sellStock(self, stock, number):
-		if type(number) not in [int]:
-			raise TypeError("Input should be integer.")
-		elif stock not in self.stock_dictionary:
-			raise ValueError("You do not have any {stock} to sell.")
-		elif self.stock_dictionary[stock] < number:
-			raise ValueError("You do not have enough stocks to sell. You can sell {} at max.".format(self.stock_dictionary[stock]))
+	def buyStock(self, number, name):
+		self.buyItem(int(number), name)
+
+	def buyMutualFund(self, number, name):
+		self.buyItem(float(number), name)
+
+	def buyBond(self, number, name):
+		self.buyItem(int(number), name)
+	#Now let's update selling methods:
+
+	def  sellItem(self, item, number, item_type):
+		if type(number) not in [int, float]:
+			raise TypeError("Stock input must be integer while mutual funds input must be float.")
+		elif item not in self.item_dictionary:
+			raise ValueError(f"You do not have any {item} to sell.")
+		elif self.item_dictionary[item] < number:
+			raise ValueError("You do not have enough {} unit item to sell. You can sell {} at max.".format(item, self.item_dictionary[item]))
 		else:
-			profit = round(random.uniform(0.5*(number*portfolio.value_dictionary[stock]), 1.5*(number*portfolio.value_dictionary[stock])),2)
-			self.addCash(profit) 
-			self.stock_dictionary.update({stock: self.stock_dictionary[stock]-number})
-		print("""\n{} cash is earned from the sale of {} number of {} stock.""".format(profit, number, stock))
+			if item_type == "stock":
+				profit = round(random.uniform(0.5*(number*portfolio.value_dictionary[item]), 1.5*(number*portfolio.value_dictionary[item])),2)
+				self.addCash(profit) 
+				self.item_dictionary.update({item: self.item_dictionary[item]-number})
+			elif item_type == "mutualfund":
+				profit = round(random.uniform(9,1.1),2)
+				self.addCash(profit) 
+				self.item_dictionary.update({item: self.item_dictionary[item]-number})
+			elif item_type == "bond":
+				profit = round(number*random.normalvariate(portfolio.value_dictionary[item], 0.1*(portfolio.value_dictionary[item])))
+				self.addCash(profit) 
+				self.item_dictionary.update({item: self.item_dictionary[item]-number})
+			else:
+				raise TypeError("Item type is not recognized. Choose either \'stock, mutualfund, or bond\'")
+		output = "{} cash is earned from the sale of {} unit of {} item.\n".format(profit, number, item)
+		now = datetime.now() 
+		date_time = now.strftime("%d/%m/%Y at %H:%M:%S.%f")
+		self.history_string += f"{date_time},\n{output}"
+	# Let's create the selling functions:
+	
+	def sellStock(self, item, number):
+		self.sellItem(item, int(number), item_type = "stock")
+
+	def sellMutualFund(self,  item, number):
+		self.sellItem(item, float(number), item_type = "mutualfund")
+
+	def sellBond(self, item, number):
+		self.sellItem(item, int(number), item_type = "bond")
+	#	Now, let's define a history method.
+
+	def history(self):
+		print(self.history_string)
 
 
 
@@ -85,11 +140,34 @@ portfolio = Portfolio() #Creates a new portfolio
 portfolio.addCash(300.50) #Adds cash to the portfolio
 # portfolio.addCash("300.50") #Shouldn't add and must throw and error message. 
 portfolio.withdrawCash(100.25) #Substract cash to the portfolio
+portfolio.history_string
+portfolio.history()
 
 #	We also need the classes of stock and mutual funds as follows:
 s = Stock(20, "HFH")
-portfolio.buyStock(5, s)
-portfolio.buyStock(5, s) #	IT WORKS
-portfolio.stock_dictionary
-portfolio.sellStock("HFH", 1) #	WWOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOWWWW, It works sooo good!
-portfolio.stock_dictionary
+portfolio.buyStock(5, s) #	WWOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOWWWW, It works sooo good!
+
+#	Now go over the mutual fund we created by inheritence:
+mf1 = MutualFund("BRT")
+mf2 = MutualFund("GHT")
+portfolio.buyMutualFund(10.3, mf1)
+portfolio.buyMutualFund(2, mf2)
+print("Cash: ", portfolio.cash, "\nAssets:", portfolio.item_dictionary)	# Cool!
+
+#	Let's look for the sell option.
+portfolio.sellMutualFund("BRT", 3) #Sells 3 shares of BRT
+portfolio.sellStock("HFH", 1) #Sells 1 share of HFH
+portfolio.withdrawCash(50) #Removes $50
+
+
+#	So far great! Now, buy and some bonds. Note that, by construct, the profit of bonds are normally distributed 
+#		with a mean value as purchase value and sd as the one tenth of the mean value.
+
+b1 = Bond(10, "Gov't Bond")
+portfolio.buyBond(4, b1)
+print("Cash: ", portfolio.cash, "\nAssets:", portfolio.item_dictionary)	# Cool!
+portfolio.sellBond("Gov't Bond", 4) #Sells 1 share of HFH
+
+
+
+portfolio.history()#Prints a list of all transactions ordered by time
