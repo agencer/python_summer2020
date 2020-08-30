610c0e94e6c9aa41671b02dca19a9ac99e3cacae
Alper Sukru Gencer <alpergencer@wust.edu>
2020-08-30 01:57:24 -0500

    Update hw_gencer.py
    Added buying and selling stocks. Also crated stock and mutual fund classes.

--- hw_gencer.py
+++ hw_gencer.py
@@ -2,28 +2,77 @@
 
 #	Let's start with our Portfolio class where we have
 #		1) cash attitude
-#		2) stock attitude
-#		3) mutual_funds attitude
-#		4) nonliquiditems dictonary for convience
+#		2) stock value attitude
+#		3) mutual_funds value attitude
 
 class Portfolio():
 	def __init__(self):
 		self.cash = float(0)
-		self.stock = 0
-		self.mutual_funds = 0
-		self.nonliquiditems = {'stock': {}, 'mutual_funds': {}}
+		self.stock_value = 0
+		self.mutualfunds_value = 0
 		#	cool! Now let's create the Porfoglio class methods for cash addition, 
 		#		substraction, and the purchase of other items.
 
 	def addCash(self, add):
 		if type(add) not in [int, float]:
-			raise TypeError("Input should be either int or add.")
+			raise TypeError("\nInput should be either integer or float.")
 		else:
 			self.cash += add
-			print("""{} unit cash is deposited.
-				\n Current deposited unit cash is {}.""".format(add, self.cash))
+			print("""\n{} unit cash is deposited.\nCurrent deposited unit cash is {}.""".format(add, self.cash))
+
+	def withdrawCash(self, remove):
+		if type(remove) not in [int, float]:
+			raise TypeError("Input should be either integer 			or ValueError.")
+		elif self.cash < remove:
+			raise ValueError("The amount desired to be withdrawn cannot be greater than the deposited cash.")
+		else:
+			self.cash -= remove
+			print("""\n{} unit cash is withdrawm.\nCurrent deposited unit cash is {}.""".format(remove, self.cash))
+		#	Yes! Now let's create functions for the stock purchase and sale:
+
+	def  buyStock(self, number, stock):
+		if type(number) not in [int]:
+			raise TypeError("Input should be integer.")
+		elif self.cash < number*stock.value:
+			raise ValueError("The amount desired to be used to purchase stocks cannot be greater than the deposited cash.")
+		else:
+			self.withdrawCash(number*stock.value)
+			self.stock_value += number*stock.value
+			print("""\n{} unit cash is withdrawm.\nCurrent deposited unit cash is {}.""".format(remove, self.cash))
+		#	Yes! Now let's create functions for the stock purchase and sale:
+
+	def  sellStock(self, number, stock):
+		if type(number) not in [int]:
+			raise TypeError("Input should be integer.")
+		elif self.stock < number*stock.value:
+			raise ValueError("The amount desired to be used to purchase stocks cannot be greater than the deposited cash.")
+		else:
+			self.withdrawCash(number*stock.value)
+			self.stock_value += number*stock.value
+			print("""\n{} unit cash is withdrawm.\nCurrent deposited unit cash is {}.""".format(remove, self.cash))
+		#	Yes! Now let's create functions for the stock purchase and sale:
+
+myfamily[]["name"]
 
 
-#	Let's try this:
+	#	Let's try this:
 portfolio = Portfolio() #Creates a new portfolio
 portfolio.addCash(300.50) #Adds cash to the portfolio
+# portfolio.addCash("300.50") #Shouldn't add and must throw and error message. 
+portfolio.withdrawCash(100.25) #Substract cash to the portfolio
+
+#	We also need the classes of stock and mutual funds as follows:
+#		s = Stock(20, "HFH")
+#		mf1 = MutualFund("BRT")
+
+class Stock():
+	def __init__(self, cost, name):
+		self.value = cost
+		self.name = name
+
+class MutualFund():
+	def __init__(self, name):
+		self.name = name
+
+portfolio.buyStock(5, s)
+
